bcdd62be54230e44e2aa598ecfc631bd955f4b8a
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-11-04 12:23:16 +0500

    Convert bot to use webhooks with Express
    Refactor bot to use webhooks with Express server and set up a webhook endpoint.

--- bot.js
+++ bot.js
@@ -1,9 +1,22 @@
 import TelegramBot from "node-telegram-bot-api";
+import express from "express";
 
 const TOKEN = "8449720717:AAFD-63utZlz0nzjo3Et8G1BJWbZBFOX-Fk";
+const URL = "https://matematika.onrender.com"; // Render sayting URL manzili
+const PORT = process.env.PORT || 3000;
 
-const bot = new TelegramBot(TOKEN, { polling: true });
+const bot = new TelegramBot(TOKEN);
+const app = express();
 
+bot.setWebHook(`${URL}/bot${TOKEN}`);
+
+app.use(express.json());
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// /start komandasi
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
@@ -11,7 +24,7 @@ bot.on("message", (msg) => {
   if (text === "/start") {
     bot.sendMessage(
       chatId,
-      `*Salom ${msg.chat.first_name}! Matematik o'yini botiga xush kelibsiz!\n\nO'yinni boshlash uchun pastdagi tugmasni bosing!*`,
+      `*Salom ${msg.chat.first_name}! Matematik o'yini botiga xush kelibsiz!\n\nO'yinni boshlash uchun pastdagi tugmani bosing!*`,
       {
         parse_mode: "Markdown",
         reply_markup: {
@@ -29,3 +42,6 @@ bot.on("message", (msg) => {
   }
 });
 
+app.listen(PORT, () => {
+  console.log(`✅ Server ${PORT} portda ishlayapti`);
+});
